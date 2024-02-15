f49a6b0a3742cd876c8918d5f41e222a9ba87344
Tarsis <br.tmvdl@gmail.com>
2024-02-15 15:54:02 -0300

    Updating app files

---
+++ src/alert.component.js
@@ -0,0 +1,11 @@
+import { HTML } from './html.js'
+
+export class nAlertComponent extends HTML {
+  getName() {
+    return 'alert-component'
+  }
+
+  setDismissible() {
+    return null
+  }
+}
---
+++ src/alert.link.component.js
@@ -0,0 +1,7 @@
+import { HTML } from './html.js'
+
+export class  nAlertLinkComponent extends HTML {
+  getName() {
+    return 'alert-link-component'
+  }
+}
