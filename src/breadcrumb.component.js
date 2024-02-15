a8190415f21a997dcba2c4a816e620d015d28dd2
Tarsis <br.tmvdl@gmail.com>
2024-02-15 15:54:30 -0300

    Updating app files

---
+++ src/breadcrumb.component.js
@@ -0,0 +1,7 @@
+import { HTML } from './html.js'
+
+export class nBreadcrumbComponent extends HTML {
+  getName() {
+    return 'breadcrumb-component'
+  }
+}
---
+++ src/breadcrumb.item.component.js
@@ -0,0 +1,7 @@
+import { HTML } from './html.js'
+
+export class nBreadcrumbItemComponent extends HTML {
+  getName() {
+    return 'breadcrumb-item-component'
+  }
+}
